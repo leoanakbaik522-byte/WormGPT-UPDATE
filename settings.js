226e5230cbcf94daa235bf1812b34f2439dde211
leoanakbaik522-byte <leoanakbaik522@gmail.com>
2025-11-29 21:56:00 +0900

    Delete settings.js

--- settings.js
+++
@@ -1,9 +0,0 @@
-/*
-Ambil apikey wormgpt di ( https://chat.wrmgpt.com )
-*/
-module.exports = {
-  token: "7850641831:AAFJM0gANGBmDedpUXkMVYmEaAAFdZLKC2U",
-  owner: 7199448431, // Ganti dengan user ID kamu
-  apiKey: "sk_live_72bdd8c9-a24f-40c2-a9a2-4ca340a8fab6e9ad", // API Key WRMGPT
-  freeLimit: 10 // Limit gratis per user per hari
-};
