c5ba48919f95774d7c2a30d9eca9ae6658f33a64
leoanakbaik522-byte <leoanakbaik522@gmail.com>
2025-11-29 21:24:02 +0900

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+/*
+Ambil apikey wormgpt di ( https://chat.wrmgpt.com )
+*/
+module.exports = {
+  token: "7850641831:AAFJM0gANGBmDedpUXkMVYmEaAAFdZLKC2U",
+  owner: 7199448431, // Ganti dengan user ID kamu
+  apiKey: "sk_live_72bdd8c9-a24f-40c2-a9a2-4ca340a8fab6e9ad", // API Key WRMGPT
+  freeLimit: 10 // Limit gratis per user per hari
+};
